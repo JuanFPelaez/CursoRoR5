e4386d507c26a67c1df04a9b16a04d756618cab7
juanfdezpelaez@gmail.com <juanfdezpelaez@gmail.com>
2018-02-15 11:27:55 +0100

    improved ej14

--- Ruby/ej14.rb
+++ Ruby/ej14.rb
@@ -1,9 +1,11 @@
 def permutations_array array
+  n_of_combinations=array.length==3 ? 6 : 24
   r=[]
-  loop do
-    r<<array.shuffle
-    break if (array.length==3 && r.uniq.length == 6) || (array.length==4 && r.uniq.length == 24)
-  end
+  r<<array.shuffle while r.uniq.size != n_of_combinations
+#con un loop do, usar while es mucho mas comodo, todo en una linea, se lee mejor
+#  loop do
+#    break if (array.length==3 && r.uniq.length == 6) || (array.length==4 && r.uniq.length == 24)
+#  end
   r.uniq.each_slice(3) do |x,y,z|
     p [x,y,z]
   end

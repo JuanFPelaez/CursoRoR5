81443872059b7dc54d1185f7a064f16ad0fe9652
juanfdezpelaez@gmail.com <juanfdezpelaez@gmail.com>
2018-02-21 10:29:20 +0100

    ej15 completo

--- ej de clase/ej15.rb
+++ ej de clase/ej15.rb
@@ -1,22 +1,14 @@
 class Restaurante
-  #attr_accessor :rice
-  def initialize menu
-    @restaurant_menu = {}
-    menu.each do |food, price|
-      @restaurant_menu[food] = price
-    end
+  def initialize precios
+    @restaurant_menu = precios
   end
 
   def coste (*pedidos)
-    #price=0
-    #pedido.each do |food, quantity|
-    #   price+=@restaurant_menu[food]*quantity
-    #end
-    #puts price
-
-    #pedido.inject {|acc, i| acc += pedido[i]*@restaurant_menu[i]}
-
-    pedidos.map {|acc, i|}
+    sum = 0
+    pedidos.map do |i|
+      i.map {|(food,quantity)| sum+=@restaurant_menu[food]*quantity}
+    end
+    puts sum
   end
 end
 
@@ -25,6 +17,6 @@ menu1 = {:rice => 3, :noodles => 2}
 menu2 = Hash[:rice,4,:noodles,5]
 
 r=Restaurante.new(precios)
-puts r.coste menu1
+r.coste menu1
 r.coste menu2
 r.coste menu1,menu2

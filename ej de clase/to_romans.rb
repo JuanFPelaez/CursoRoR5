81dd58929232aec82b417ce237fd25190eb0c7c5
juanfdezpelaez@gmail.com <juanfdezpelaez@gmail.com>
2018-02-22 12:35:27 +0100

    to_romans_v2 completado, pendiente version 3 recursiva

--- ej de clase/to_romans.rb
+++ ej de clase/to_romans.rb
@@ -59,33 +59,34 @@ class Integer #extendemos la clase de enteros
   def to_romans_v2
     puts "Entero inválido" unless to_romans_rango?self
     #r es el resultado
-    r=[]
     #h es el hash de claves
-    h=Hash[1000,'M',500,'D',100,'C',50,'L',10,'X',100,'C',5,'V',1,'I']
-    h.each do |index, code????|
-    r<<(self/1000).times h[1000]
-    #unidades, casos
-    #1->I 4->IV 5->V 9->IX --> rango 1-3: I,II,III; 4:IV; 5:V; 6-8:VI,VII,VIII, 9:IX
-    #unidades: modulo 10, decenas k/10%10, centenas k/10²%10, millares k/10⁴%10
+    number=self
+    h=Hash[1000,'M',900,'CM',500,'D',400,'CD',100,'C',90,'XC',50,'L',40,'XL',10,'X',9,'IX',5,'V',4,'IV',1,'I']
+    h.inject("") do |r,(k,v)|
+      n_letters, number = number.divmod(k)
+      r << v*n_letters
+    end
   end
 end
 
-def test
-  i=3550
+def test i,j
   n=i
   loop do
-    puts "#{n} -> #{n.to_romans}"
+    puts "#{n} -> #{n.to_romans_v2}"
     n+=1
-    break if n==i+500
+    break if n==j
   end
 end
 
 def main
   loop do
     puts "Introduce un entero inferior a 4000 para pasar a romanos"
-    puts "#{m=gets.chomp.to_i.to_romans_v2 unless m=='test'} \r\n\r\n"
+    m=gets.chomp
+    puts "#{m.to_i.to_romans_v2 unless m=='test'} \r\n\r\n"
     puts test if m=='test'
   end
 end
-main
-#test
+# puts 1234.to_romans_v2
+# main
+puts "Introduce el rango inferior y despues el rango superior"
+test gets.chomp.to_i, gets.chomp.to_i

123731db73820368c32e2a3aaed196483701d6e8
juanfdezpelaez@gmail.com <juanfdezpelaez@gmail.com>
2018-02-26 09:57:46 +0100

    ej5.rb a medias

--- ej de clase/+5.rb
+++ ej de clase/+5.rb
@@ -1,9 +1,9 @@
 def murcielago frase
   murcielago_dict=Hash["M",0,"U",1,"R",2,"C",3,"I",4,"E",5,"L",6,"A",7,"G",8,"O",9]
-  frase.split("").inject("") do |r,i|
-    r<<murcielago_dict[i] if murcielago_dict.keys.include? i
-    r<<i unless murcielago_dict.keys.include? i
+  puts frase.split("").inject("") do |r,i|
+     r<<murcielago_dict[i] if murcielago_dict.keys.include? i
+     r<<i unless murcielago_dict.keys.include? i
   end
 end
 
-puts murcielago "POTATOMAN"
+puts murcielago "POTATOMAN ES LO MAS TOP"

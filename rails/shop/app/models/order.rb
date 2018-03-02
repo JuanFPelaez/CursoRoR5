ee57589195a2e72c4ab3ce43941af39d6886cf97
juanfdezpelaez@gmail.com <juanfdezpelaez@gmail.com>
2018-03-02 12:59:18 +0100

    tema 8 completado

--- rails/shop/app/models/order.rb
+++ rails/shop/app/models/order.rb
@@ -1,3 +1,6 @@
 class Order < ApplicationRecord::Base
+  has_many :line_items, dependent: :destroy
   PAYMENT_TYPES = ["Check", "Credit card", "Purchase Order"]
+  validates :name, :adress, :email, presence:true
+  validates :pay_type, inclusion: PAYMENT_TYPES
 end

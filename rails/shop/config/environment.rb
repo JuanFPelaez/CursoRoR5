61aa6cfd69d2ae7dfa9a1814f2aa067f157e1325
juanfdezpelaez@gmail.com <juanfdezpelaez@gmail.com>
2018-03-05 10:09:25 +0100

    tema 10 completo

--- rails/shop/config/environment.rb
+++ rails/shop/config/environment.rb
@@ -3,3 +3,16 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Shop::Application.configure do
+  config.action_mailer.delivery_method = :smtp #smpt para enviar, test para salida por consola
+  config.action_mailer.smtp_settings ={
+    adress: "smtp.gmail.com",
+    port:587,
+    domain: "gmail.com",
+    authentication: "plain",
+    user_name: "juanxtrvideos@gmail.com",
+    password: "Juan588266",
+    enable_starttls_auto: true
+  }
+end

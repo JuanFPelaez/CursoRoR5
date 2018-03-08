09ca23055ee108cc953bddecd275f7fb345766f8
juanfdezpelaez@gmail.com <juanfdezpelaez@gmail.com>
2018-03-08 09:42:49 +0100

    fixed redundant git folder

--- rails/shop/app/controllers/users_controller.rb
+++ rails/shop/app/controllers/users_controller.rb
@@ -71,7 +71,7 @@ end
     def set_user
       @user = User.find(params[:id])
     end
-
+    
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:name, :password, :password_confirmation)

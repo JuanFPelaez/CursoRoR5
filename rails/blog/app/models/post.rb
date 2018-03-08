7b48b559f8562c13706e78a4904e6d23cf5d96c6
juanfdezpelaez@gmail.com <juanfdezpelaez@gmail.com>
2018-03-08 11:06:24 +0100

    basic validation included

--- rails/blog/app/models/post.rb
+++ rails/blog/app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+  validates :title, :introduction, :content, presence: true
+  validates :image_url, allow_blank: true, format: {
+  with: %r{\.(gif|jpg|png)\Z}i,
+  messsage:'must be a URL for GIF, JPG or PNG image.'
+  }
 end

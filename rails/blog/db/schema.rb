04bf7d8015f7e4e8a05ca05b8e661190f1af4f55
JuanFPelaez <jfdezpelaez@gmail.com>
2018-03-11 12:01:12 +0100

    Users created

--- rails/blog/db/schema.rb
+++ rails/blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180307100352) do
+ActiveRecord::Schema.define(version: 20180311103609) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 20180307100352) do
     t.text "content"
     t.string "image_url"
     t.string "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
